5ba8625c190464e29e4d03e4ea9c17b636d59303
Gleb <gleb@grib.in>
2023-09-17 18:48:01 +0300

    add crash test

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ mongoose.connect(DB_URL, {
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 
 app.use(errorLogger);
